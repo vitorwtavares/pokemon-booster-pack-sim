3319f81762a2fae7ce5707e8e8585e7b790f4640
vitorwtavares <vitortavares_@hotmail.com>
2023-04-09 00:43:47 -0300

    fix: refactor generateRandomCardIds to avoid duplicate values, fix default pack size

--- src/utils/getRandomCardIds.ts
+++ src/utils/getRandomCardIds.ts
@@ -1,7 +1,13 @@
+const generateRandomNumberOnRange = (max: number, min: number = 1) =>
+  Math.floor(Math.random() * max + min)
+
 export const getRandomCardIds = (packSize: number, amountOfCards: number) => {
-  const randomIds = Array(amountOfCards)
-    .fill(0)
-    .map(() => Math.floor(Math.random() * (packSize - 1) + 1))
+  let randomIds: number[] = []
+
+  while (randomIds.length < amountOfCards) {
+    const randomNumber = generateRandomNumberOnRange(packSize)
+    if (!randomIds.includes(randomNumber)) randomIds.push(randomNumber)
+  }
 
   if (amountOfCards === 1) return `number:${[...randomIds]}`
 
