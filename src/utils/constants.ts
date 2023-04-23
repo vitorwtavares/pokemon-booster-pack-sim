d050964188ea33bbf41b17c5a239f7925a1f4f0c
vitorwtavares <vitortavares_@hotmail.com>
2023-04-23 10:31:57 -0300

    fix total packs constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,11 @@
+import { excludedPacksNamesList } from './excludedPacksNamesList'
+
 export const DEFAULT_SELECTED_BOOSTER_PACK_ID = 'base1'
 export const DEFAULT_SELECTED_BOOSTER_PACK_CARDS_AMOUNT = 102
 
 export const DEFAULT_PACKS_PAGE_SIZE = 6
 
-export const DEFAULT_PACKS_TOTAL = 151
+export const DEFAULT_PACKS_TOTAL = 151 - excludedPacksNamesList.length
 
 export const DEFAULT_PACKS_LAST_PAGE = Math.ceil(
   DEFAULT_PACKS_TOTAL / DEFAULT_PACKS_PAGE_SIZE
