ae6ea5ea285c025d1f29fb0b8249ac28be754cef
Vítor Tavares <vitortavares_@hotmail.com>
2026-04-22 14:15:00 -0300

    feat(pack-opening): add PackOpening idle phase with pack art and swipe hint

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -1,78 +1,6 @@
-import { Button, Flex } from '@chakra-ui/react'
-import { css } from '@emotion/react'
+import { Flex } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import { DESKTOP_HEADER_HEIGHT } from './utils/constants'
-
-const OpenPackButtonVariablesAndAnimations = css`
-  --red: #f80e35;
-  --yellow: #eedf10;
-  --green: #21e985;
-  --blue: #0dbde9;
-  --violet: #c929f1;
-
-  @keyframes animatedBackground {
-    0% {
-      background-image: repeating-linear-gradient(
-        120deg,
-        var(--yellow),
-        var(--red),
-        var(--violet),
-        var(--blue),
-        var(--green)
-      );
-    }
-    20% {
-      background-image: repeating-linear-gradient(
-        120deg,
-        var(--red),
-        var(--violet),
-        var(--blue),
-        var(--green),
-        var(--yellow)
-      );
-    }
-    40% {
-      background-image: repeating-linear-gradient(
-        120deg,
-        var(--violet),
-        var(--blue),
-        var(--green),
-        var(--yellow),
-        var(--red)
-      );
-    }
-    60% {
-      background-image: repeating-linear-gradient(
-        120deg,
-        var(--blue),
-        var(--green),
-        var(--yellow),
-        var(--red),
-        var(--violet)
-      );
-    }
-    80% {
-      background-image: repeating-linear-gradient(
-        120deg,
-        var(--green),
-        var(--yellow),
-        var(--red),
-        var(--violet),
-        var(--blue)
-      );
-    }
-    100% {
-      background-image: repeating-linear-gradient(
-        120deg,
-        var(--yellow),
-        var(--red),
-        var(--violet),
-        var(--blue),
-        var(--green)
-      );
-    }
-  }
-`
 
 export const HeaderAndContentContainer = styled(Flex)`
   flex-direction: column;
@@ -82,43 +10,11 @@ export const HeaderAndContentContainer = styled(Flex)`
 
 export const ContentWrapper = styled(Flex)`
   width: 100%;
-  gap: 50px;
-  max-width: 1024px;
-  min-height: calc(100vh - ${DESKTOP_HEADER_HEIGHT});
+  height: calc(100vh - ${DESKTOP_HEADER_HEIGHT});
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   padding: 40px 0;
-
-  @media only screen and (max-width: 991px) {
-    min-height: 100%;
-  }
-`
-
-export const OpenPackButton = styled(Button)`
-  ${OpenPackButtonVariablesAndAnimations}
-
-  width: 300px;
-  color: #fff;
-
-  background-image: repeating-linear-gradient(
-    120deg,
-    var(--yellow),
-    var(--red),
-    var(--violet),
-    var(--blue),
-    var(--green)
-  );
-  filter: saturate(90%);
-
-  :hover {
-    filter: saturate(100%);
-    animation: animatedBackground 0.5s linear infinite;
-  }
-
-  @media only screen and (max-width: 991px) {
-    margin-top: 30px;
-  }
 `
--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,14 @@
-import { useContext, useState } from 'react'
-
-import { Backdrop, CardPack, Header, Credits } from '@/components'
-
-import { SelectedPackContext } from '@/context/SelectedPack'
-import { usePackCardsQuery } from '@/hooks/usePackCardsQuery'
-
-import { CARDS_PER_PACK } from '@/utils/constants'
+import { Backdrop, Credits, Header, PackOpening } from '@/components'
 
 import * as S from '@/App.styles'
 
-const EMPTY_CARDS = Array(CARDS_PER_PACK).fill(undefined)
-
 const App = () => {
-  const { selectedPack } = useContext(SelectedPackContext)
-  const [isCardFlipped, setIsCardFlipped] = useState(false)
-
-  const {
-    data: cards,
-    refetch,
-    isFetching,
-  } = usePackCardsQuery(selectedPack.id)
-
-  const showFlipped = isCardFlipped && !!cards
-
-  const handleClick = async () => {
-    if (showFlipped) setIsCardFlipped(false)
-
-    const result = await refetch()
-
-    if (result.data) {
-      setTimeout(() => setIsCardFlipped(true), 500)
-    }
-  }
-
   return (
     <S.HeaderAndContentContainer>
       <Backdrop />
       <Header />
       <S.ContentWrapper>
-        <S.OpenPackButton onClick={handleClick} loading={isFetching}>
-          {showFlipped ? 'Open another pack' : 'Open pack'}
-        </S.OpenPackButton>
-        <CardPack cards={cards ?? EMPTY_CARDS} isCardFlipped={showFlipped} />
+        <PackOpening />
         <Credits />
       </S.ContentWrapper>
     </S.HeaderAndContentContainer>
